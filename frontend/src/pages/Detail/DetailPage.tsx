9100fd0cac7c45d077d199de8bf0370b751d594c
abelyon <lehotai2006@gmail.com>
2026-06-29 05:36:53 +0200

    fix(detail): use symmetric px-5 on horizontal scroll rows

--- frontend/src/pages/Detail/DetailPage.tsx
+++ frontend/src/pages/Detail/DetailPage.tsx
@@ -522,7 +522,7 @@ const DetailPage = () => {
               <SectionHeader title="Cast" emphasized />
               {cast.length ? (
                 <div className="-mx-5 overflow-x-auto [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden">
-                  <div className="flex gap-5 pl-5 pb-1">
+                  <div className="flex gap-5 px-5 pb-1">
                     {cast.map((person) => (
                       <CastPill
                         key={`cast-${person.id}`}
@@ -530,7 +530,6 @@ const DetailPage = () => {
                         onSelect={() => navigate(`/person/${person.id}`)}
                       />
                     ))}
-                    <div className="shrink-0 w-5" aria-hidden />
                   </div>
                 </div>
               ) : (
@@ -576,7 +575,7 @@ const DetailPage = () => {
               <section className="flex flex-col gap-3">
                 <SectionHeader title="More like this" />
                 <div className="-mx-5 overflow-x-auto [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden">
-                  <div className="flex gap-5 pl-5 pb-1">
+                  <div className="flex gap-5 px-5 pb-1">
                     {recommendationItems.map((item) => (
                       <div key={`reco-${item.media_type}-${item.id}`} className="w-44 shrink-0">
                         <MediaCard
@@ -586,7 +585,6 @@ const DetailPage = () => {
                         />
                       </div>
                     ))}
-                    <div className="shrink-0 w-5" aria-hidden />
                   </div>
                 </div>
               </section>
